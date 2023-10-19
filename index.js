b0e3eb998050aa27505d38d11d2a99d8b0994252
Dipto Shaha <diptoshaha2635@gmail.com>
2023-10-19 16:50:19 +0600

    Product Updated Successfully

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port= process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 require('dotenv').config()
 
 
@@ -33,39 +34,62 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+
+    // Selected brand Produt
     app.get(`/branddetails/:brand`, async (req, res) => {
-      const result = await Collection.find().toArray();
+      const brand=req.params.brand;
+      console.log("Single Barand all Product request ",brand);
+      const result = await Collection.find({brand:brand}).toArray();
       res.send(result);
-      });
+    });
+
+    // Selected Single Produt
     app.get('/productDetails/:_id',async(req,res)=>{
       const id = req.params._id;
+      console.log("Single Product request " ,id);
       const result = await Collection.findOne({ _id: new ObjectId(id) });
       return res.send(result);
 
     })
-
+    // Cart Produt
     app.get('/saveCartProduct/:userId',async(req,res)=>{
       const userId= req.params.userId;
-      console.log("I am here",userId);
+      console.log("Cart Prouct get by mail address ",userId);
       const result = await CollectionCartProduct.find({ user_id: userId }).toArray();
       return res.send(result);
 
     })
+    //Add Product
     app.post("/addproduct", async (req, res) => {
       const product = req.body;
+      console.log("Product Add request");
       const result = await Collection.insertOne(product);
       res.send(result);
     });
+    //Save Cart Product
     app.post("/saveCartProduct",async(req,res)=>{
       const cartProduct = req.body;
-      console.log(cartProduct);
+      console.log("Save Cart Prouct with mail address ",cartProduct);
       const result = await CollectionCartProduct.insertOne(cartProduct);
       res.send(result);
     })
-
+    app.put("/updateProduct/:_id", async (req, res) => {
+      const id = req.params._id;
+      console.log("Update Product ",id);
+      const product = req.body;
+      console.log(id);
+      const result = await Collection.updateOne(
+        { _id: new ObjectId(id) }, // Find Data by query many time query type is "_id: id" Cheack on database
+        {
+          $set: product, // Set updated Data
+        },
+        { upsert: true } // define work
+      );
+      res.send({ result });
+    });
     app.delete("/cartDelete/:_id", async (req, res) => {
       const id = req.params._id;
-      console.log(id);
+      console.log("Product delted from Cart request ",id);
       const result = await CollectionCartProduct.deleteOne({productId: id });
       res.send(result);
     });
